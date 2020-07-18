4f492f99c167c6669401447a937770c5786a90fe
Matt Stavinga <mattstavinga@gmail.com>
2020-07-17 23:21:29 -0400

    minor adjustment

--- src/main/java/com/beerme/controller/BreweryController.java
+++ src/main/java/com/beerme/controller/BreweryController.java
@@ -17,13 +17,16 @@ public class BreweryController {
 		this.breweryDAO = breweryDAO;
 	}
 	
-	
-	
-	
-	@RequestMapping(path = "/brewery", method = RequestMethod.GET)
-	public List<Brewery> getBreweries() {
-		List<Brewery> breweryList = breweryDAO.viewAllBreweries();
-		return breweryList;
+	@RequestMapping(path = "", method = RequestMethod.GET)
+	public String testMethod() {
+		return "hello world";
 	}
+	
+	
+//	@RequestMapping(path = "/brewery", method = RequestMethod.GET)
+//	public List<Brewery> getBreweries() {
+//		List<Brewery> breweryList = breweryDAO.viewAllBreweries();
+//		return breweryList;
+//	}
 	
 }
